953b3c8542e2fcc3cd500b3275b7fe71c8002566
Dylan Bailey <dylanba1@gmail.com>
2018-02-08 10:07:24 -0500

    Add files via upload

---
+++ mybreakout.py
@@ -0,0 +1,15 @@
+import pygame,random,time
+py.game(init)
+
+gwidth = 640
+gheight = 480
+clock=pygame.time.Clock()
+screen=pygame.display.set_mode([gwidth,gheight])
+pygame.display.set_caption('Jail Break')
+
+
+#put the colors for tiles here
+
+#sets width and height
+
+def text
