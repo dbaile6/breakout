466bec318ac887f2504f1469d830c2378d7022fb
Dylan Bailey <dylanba1@gmail.com>
2018-02-08 13:40:49 -0500

    Add files via upload

--- mybreakout.py
+++ mybreakout.py
@@ -1,7 +1,31 @@
+#Quicksand font by Andrew Pagliwanan
+#Background image provided by Unlucky Software
+#Original music by Dylan Bailey
+
+
 import pygame, sys
+import py
+
+#Code for Music
 pygame.init()
+pygame.mixer.init()
+song = 'music.ogg'
+pygame.mixer.music.load(song)
+pygame.mixer.music.play(loops=-1, start=0.0)
+
+# Initialize display
 screen = pygame.display.set_mode([640,480])
+
+#Defines custom font
+
+myfont = pygame.font.SysFont("Quicksand-Bold.otf", 25)
+
+# Reference colors
+
 black = [0, 0, 0]
+red = [255, 0, 0]
+
+#Create characteristics of bricks
 
 class Brick(pygame.sprite.Sprite):
     image = None
@@ -12,15 +36,21 @@ class Brick(pygame.sprite.Sprite):
         if Brick.image is None:
                 Brick.image = pygame.image.load("brick.png")
         self.image = Brick.image
-
-        # Make our top-left corner the passed-in location.
         self.rect = self.image.get_rect()
         self.x = x
         self.y = y
         self.rect.topleft = (self.x, self.y)
 
+#Settings for text display
+def msg(txt,color,size,x,y):
+    font=pygame.font.SysFont("bold",size)
+    msgtxt=font.render(txt,True,color)
+    msgrect=msgtxt.get_rect()
+    msgrect.center=x,y
+    screen.blit(msgtxt,msgrect)
 
-#the game's variables
+# Create ball movement settings
+
 ball_x = 50
 ball_y = 100
 ball_radius = 10
@@ -28,14 +58,17 @@ ball_color = [222,50,50]
 ball_speed_x = 3
 ball_speed_y = 5
 
+# Movement settings for the paddle
+
 paddle_x = 20
 paddle_y = 450
 paddle_width = 60
 paddle_height = 20
-paddle_color = [20,180,180]
+paddle_color = [64,100,41]
 paddle_speed = 10
 
-myfont = pygame.font.SysFont("Arial", 22)
+#Score to be tracked
+
 score = 0
 
 brick_array = []
@@ -44,53 +77,49 @@ for i in range(1,8):
     brick_array.append(brick1)
 
 
-running = True
+pygame.key.set_repeat(20, 20)
 
-#Main game loop
+running = True
 while running:
     for event in pygame.event.get():
-        #check if you've exited the game
         if event.type == pygame.QUIT:
             running = False
 
         if event.type == pygame.MOUSEMOTION:
-            coordinates = pygame.mouse.get_pos() #gives (x,y) coordinates
-            paddle_x = coordinates[0] - paddle_width/2 #sets the paddle_x variable to the first item in coordinates
+            coordinates = pygame.mouse.get_pos()
+            paddle_x = coordinates[0] - paddle_width/2
             if paddle_x < 0:
                 paddle_x = 0
             if paddle_x > screen.get_width() - paddle_width:
                 paddle_x = screen.get_width() - paddle_width
 
-        if event.type == 
 
 
-
-    #pause for 20 milliseconds
-    pygame.time.delay(20)
-    #make the screen completely white
+    pygame.time.delay(10)
     screen.fill(black)
+    msg("Jailbreak",red,40,200,100)
+    icon=pygame.image.load("background.jpg")
+    icon=pygame.transform.scale(icon,[640,480])
+    screen.blit(icon,[1, 1])
+    wait=1
 
-    #move the ball
     ball_y = ball_y + ball_speed_y
     ball_x = ball_x + ball_speed_x
-    #check if the ball is off the bottom of the screen
     if ball_y > screen.get_height() - ball_radius:
         ball_speed_y = -ball_speed_y
-        #do something different
-    #check if the ball hit the top of the screen
+
     if ball_y < ball_radius:
         ball_speed_y = -ball_speed_y
-    #check if the ball hit the left side of the screen
     if ball_x < ball_radius:
         ball_speed_x = -ball_speed_x
-    #check if the ball hit the right side of the screen
+
     if ball_x > screen.get_width() - ball_radius:
         ball_speed_x = -ball_speed_x
 
-    #create imaginary rectangles around ball and paddle
+
     ball_rect = pygame.Rect(ball_x-ball_radius, ball_y-ball_radius, ball_radius*2,ball_radius*2) #circles are measured from the center, so have to subtract 1 radius from the x and y
     paddle_rect = pygame.Rect(paddle_x, paddle_y, paddle_width, paddle_height)
-    #see if the rectangles overlap
+
     if ball_rect.colliderect(paddle_rect):
         ball_speed_y = -ball_speed_y
 
@@ -101,15 +130,14 @@ while running:
             ball_speed_y = - ball_speed_y
 
 
-    #draw everything on the screen
     score_label = myfont.render(str(score), 1, pygame.color.THECOLORS['white'])
     screen.blit(score_label, (5, 10))
     for brick in brick_array:
         screen.blit(brick.image, brick.rect)
     pygame.draw.circle(screen, ball_color, [int(ball_x), int(ball_y)], ball_radius, 0)
     pygame.draw.rect(screen, paddle_color, [paddle_x, paddle_y, paddle_width, paddle_height], 0)
-    #update the entire display
     pygame.display.update()
 
 
+pygame.mixer.stop()
 pygame.quit()
